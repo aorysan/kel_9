ad99452648901ffef2933d2ab2303f724194270a
aorysan <aryoadiputro@gmail.com>
2023-09-06 10:33:58 +0700

    link

--- kalkulator.java
+++ kalkulator.java
@@ -5,5 +5,6 @@ public class kalkulator {
 
     public static void main (String[] args) {
         System.out.print("tugas kelompok dengan topik kalkulator");
+        System.out.print("//trello.com/invite/b/NmAuoSQN/ATTI432ffd3992e0bb3d063f066d84ff51aa9C3FE394/kalkulator");
     }
 }
